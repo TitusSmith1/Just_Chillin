79ba18da542a61dec70efa9e76bdf1d195951961
TitusSmith1 <72360616+TitusSmith1@users.noreply.github.com>
2024-10-23 01:24:45 -0600

    Started Repository

---
+++ main/handleHttp.ino
@@ -0,0 +1,408 @@
+/** Handle root or redirect to captive portal */
+void handleRoot() {
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return;
+  }
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.sendContent(
+    "   <!DOCTYPE html>"
+    "    <html lang='en'>"
+    "    <head>"
+    "        <meta charset='UTF-8'>"
+    "        <meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+    "       <title>JustChillin'Freezer</title>"
+    "        <style>"
+    "            body {"
+    "                font-family: Arial, sans-serif;"
+    "                margin: 0;"
+    "                padding: 20px;"
+    "                background-color: #0f0f0f;"
+    "                color: #333;"
+    "            }"
+    "            h1 {"
+		"    padding: 20px;"
+		"    size: 60px;"
+		"    margin: 0;"
+    "                color: #0194B0;"
+    "            }"
+		"h2 {"
+    "                font-family: Arial, sans-serif;"
+    "                margin: 0;"
+    "                padding: 30px;"
+		"    size: 20px;"
+    "                background-color: #3f3f3f;"
+    "                color: #0194B0;"
+		"    border-radius: 10px;"
+    "                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);"
+    "            }"
+		"h3 {"
+    "                font-family: Arial, sans-serif;"
+    "                margin: 10;"
+    "                padding: 15px;"
+		"    size: 20px;"
+    "                background-color: white;"
+    "                color: #000000;"
+		"    border-radius: 10px;"
+    "                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);"
+    "            }"
+		"h4 {"
+    "                font-family: Arial, sans-serif;"
+    "                margin: 10;"
+    "                padding: 15px;"
+		"    size: 15px;"
+    "                background-color: white;"
+    "                color: #333;"
+		"    border-radius: 10px;"
+    "                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);"
+    "            }"
+    "green_text {" 
+    "                color: #00ff00;"
+    "            }"
+    "red_text {" 
+    "                color: #ff0000;"
+    "            }"
+    "            .container {"
+    "                max-width: 600px;"
+    "                margin: auto;"
+    "                padding: 20px;"
+    "                background: white;"
+    "                border-radius: 10px;"
+    "                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);"
+    "            }"
+    "            img {"
+    "                max-width: 100%;"
+    "                height: auto;"
+    "                border-radius: 8px;"
+    "            }"
+    "            a {"
+    "                color: #0194B0;"
+    "                text-decoration: none;"
+    "            }"
+    "            a:hover {"
+    "                text-decoration: underline;"
+    "            }"
+    "            @media (max-width: 600px) {"
+    "                body {"
+    "                    padding: 10px;"
+    "                }"
+    "            }"
+    "        </style>"
+    "    </head>"
+    "    <body>"
+    "        <div class='container'>"
+    "            <h1>Just Chillin' Freezer</h1>"
+    "            <img src='https://img.goodfon.com/wallpaper/nbig/4/57/fjords-rocky-mountains-snow-sea-bay-water-night-northern-lig.webp' alt='Background_Image'>"
+  );
+  server.sendContent("<h2>Current Freezer Temperature: " + String(temp) + " *C</h2>");
+  if(is_active){
+    server.sendContent("<h2>Freezer Status <green_text> ON </green_text> </h2>");
+  }
+  else{
+    server.sendContent("<h2>Freezer Status <red_text> OFF </red_text> </h2>");
+  }
+  server.sendContent("<p><h3>You can update the setpoint temperature <a href='/set_temp'>here</a>.</h3></p><p><h3>You can view the temperature graph <a href='/get_graph'>here</a>.</h3></p><p><h4> Wifi Connection:</p>");
+  if (server.client().localIP() == apIP) {
+    server.sendContent(String("<p>You are connected through the soft AP: <strong>") + softAP_ssid + "</strong></p>");
+  } else {
+    server.sendContent(String("<p>You are connected through the wifi network: <strong>") + ssid + "</strong></p>");
+  }
+  server.sendContent("<p>Wifi connection configuration <a href='/wifi'>here</a>.</h4></p></body></html></div></body></html>");
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+/** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
+boolean captivePortal() {
+  if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname)+".local")) {
+    Serial.print("Request redirected to captive portal");
+    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+    server.send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+    server.client().stop(); // Stop is needed because we sent no content length
+    return true;
+  }
+  return false;
+}
+
+/** Wifi config page handler */
+void handleWifi() {
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.sendContent(
+    "<html><head></head><body>"
+    "<h1>Wifi config</h1>"
+  );
+  if (server.client().localIP() == apIP) {
+    server.sendContent(String("<p>You are connected through the soft AP: ") + softAP_ssid + "</p>");
+  } else {
+    server.sendContent(String("<p>You are connected through the wifi network: ") + ssid + "</p>");
+  }
+  server.sendContent(
+    "\r\n<br />"
+    "<table><tr><th align='left'>SoftAP config</th></tr>"
+  );
+  server.sendContent(String() + "<tr><td>SSID " + String(softAP_ssid) + "</td></tr>");
+  server.sendContent(String() + "<tr><td>IP " + toStringIp(WiFi.softAPIP()) + "</td></tr>");
+  server.sendContent(
+    "</table>"
+    "\r\n<br />"
+    "<table><tr><th align='left'>WLAN config</th></tr>"
+  );
+  server.sendContent(String() + "<tr><td>SSID " + String(ssid) + "</td></tr>");
+  server.sendContent(String() + "<tr><td>IP " + toStringIp(WiFi.localIP()) + "</td></tr>");
+  server.sendContent(
+    "</table>"
+    "\r\n<br />"
+    "<table><tr><th align='left'>WLAN list (refresh if any missing)</th></tr>"
+  );
+  Serial.println("scan start");
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n > 0) {
+    for (int i = 0; i < n; i++) {
+      server.sendContent(String() + "\r\n<tr><td>SSID " + WiFi.SSID(i) + String((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":" *") + " (" + WiFi.RSSI(i) + ")</td></tr>");
+    }
+  } else {
+    server.sendContent(String() + "<tr><td>No WLAN found</td></tr>");
+  }
+  server.sendContent(
+    "</table>"
+    "\r\n<br /><form method='POST' action='wifisave'><h4>Connect to network:</h4>"
+    "<input type='text' placeholder='network' name='n'/>"
+    "<br /><input type='password' placeholder='password' name='p'/>"
+    "<br /><input type='submit' value='Connect/Disconnect'/></form>"
+    "<p>You may want to <a href='/'>return to the home page</a>.</p>"
+    "</body></html>"
+  );
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+/** Handle the WLAN save form and redirect to WLAN config page again */
+void handleWifiSave() {
+  Serial.println("wifi save");
+  server.arg("n").toCharArray(ssid, sizeof(ssid) - 1);
+  server.arg("p").toCharArray(password, sizeof(password) - 1);
+  server.sendHeader("Location", "wifi", true);
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 302, "text/plain", "");  // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.client().stop(); // Stop is needed because we sent no content length
+  saveCredentials();
+  connect = strlen(ssid) > 0; // Request WLAN connect with new credentials if there is a SSID
+}
+
+void handleSetTemp() {
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  server.sendContent(R"(
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Just Chillin' Freezer</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #0f0f0f;
+                color: #333;
+            }
+            h1 {
+                padding: 20px;
+                margin: 0;
+                color: #0194B0;
+            }
+            h4 {
+                margin: 10;
+                padding: 15px;
+                size: 15px;
+                background-color: white;
+                color: #333;
+                border-radius: 10px;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            }
+            .container {
+                max-width: 600px;
+                margin: auto;
+                padding: 20px;
+                background: white;
+                border-radius: 10px;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            }
+            input[type="text"], input[type="submit"] {
+                padding: 10px;
+                margin-top: 10px;
+                border-radius: 5px;
+                border: 1px solid #ccc;
+                width: calc(100% - 22px);
+            }
+            input[type="submit"] {
+                background-color: #0194B0;
+                color: white;
+                cursor: pointer;
+            }
+            input[type="submit"]:hover {
+                background-color: #017f9b;
+            }
+        </style>
+    </head>
+    <body>
+        <div class='container'>
+            <h1>Just Chillin' Freezer</h1>
+            <form method="POST" action="save_temp">
+                <h4>Send Data:</h4>
+                <input type="text" placeholder="Temperature" name="t" required pattern="^\d+(\.\d+)?$" title="Enter a valid decimal number" />
+                <br />
+                <input type="submit" value="Send" />
+            </form>
+        </div>
+    </body>
+    </html>
+    )");
+    /*
+    "<html><head></head><body>"
+    "<h1>Display</h1>"
+  );
+    server.sendContent(
+    "</table>"
+    "\r\n<br /><form method='POST' action='save_temp'><h4>Send Data:</h4>"
+    "<input type='number' placeholder='temperature' name='t'/>"
+    "<br /><input type='submit' value='Send'/></form>"
+    "</body></html>"
+  );*/
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+
+void handleSaveTemp() {
+  Serial.println("Data Incoming");
+  newtemp=server.arg("t").toFloat();
+  Serial.println(newtemp);
+
+
+  // Create a small HTML response with a JavaScript alert
+  String response = "<html><head><script>"
+                    "alert('Temp updated: " + String(newtemp) + "');"
+                    "window.location.href = 'set_temp';"
+                    "</script></head><body></body></html>";
+
+  // Send the response
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send(200, "text/html", response); // Send a 200 response with the HTML
+  server.client().stop(); // Stop is needed because we sent no content length
+  updateOLED();
+}
+/*
+void handleGraph(){
+  server.sendContent(R"(
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Temperature Graph</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 20px;
+                background-color: #0f0f0f;
+                color: #333;
+            }
+            h1 {
+                padding: 20px;
+                margin: 0;
+                color: #0194B0;
+            }
+            .container {
+                max-width: 600px;
+                margin: auto;
+                padding: 20px;
+                background: white;
+                border-radius: 10px;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            }
+            canvas {
+                max-width: 100%;
+                border-radius: 8px;
+            }
+        </style>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    </head>
+    <body>
+        <div class='container'>
+            <h1>Temperature Over Time</h1>
+            <canvas id="temperatureChart"></canvas>
+            <script>
+                // Fetch temperature data from the server
+                fetch('/get_temperature_data')
+                    .then(response => response.json())
+                    .then(data => {
+                        const labels = ) + labels + (;
+                        const temperatureChart = new Chart(
+                            document.getElementById('temperatureChart'),
+                            {
+                                type: 'line',
+                                data: {
+                                    labels: labels,
+                                    datasets: [{
+                                        label: 'Temperature (°C)',
+                                        data: data,
+                                        borderColor: '#0194B0',
+                                        backgroundColor: 'rgba(1, 148, 176, 0.2)',
+                                        fill: true,
+                                    }]
+                                },
+                                options: {
+                                    responsive: true,
+                                    scales: {
+                                        y: {
+                                            beginAtZero: true
+                                        }
+                                    }
+                                }
+                            }
+                        );
+                    });
+            </script>
+        </div>
+    </body>
+    </html>
+    )");
+  server.client().stop(); // Stop is needed because we sent no content length
+}
+*/
+void handleNotFound() {
+  if (captivePortal()) { // If caprive portal redirect instead of displaying the error page.
+    return;
+  }
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 404, "text/plain", message );
+}
+
---
+++ main/main.ino
@@ -0,0 +1,248 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <DNSServer.h>
+#include <ESPmDNS.h>
+#include <Preferences.h>
+// Web server
+WebServer server(80);
+
+
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+#include <Adafruit_MAX31865.h>
+
+// Use software SPI: CS, DI, DO, CLK
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(13, 14,27, 26);
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
+
+/*
+ * This example serves a "hello world" on a WLAN and a SoftAP at the same time.
+ * The SoftAP allow you to configure WLAN parameters at run time. They are not setup in the sketch but saved on EEPROM.
+ * 
+ * Connect your computer or cell phone to wifi network ESP_ap with password 12345678. A popup may appear and it allow you to go to WLAN config. If it does not then navigate to http://192.168.4.1/wifi and config it there.
+ * Then wait for the module to connect to your wifi and take note of the WLAN IP it got. Then you can disconnect from ESP_ap and return to your regular WLAN.
+ * 
+ * Now the ESP8266/ESP32 is in your network. You can reach it through http://192.168.x.x/ (the IP you took note of) or maybe at http://esp8266.local too.
+ * 
+ * This is a captive portal because through the softAP it will redirect any http request to http://192.168.4.1/
+ */
+
+/* Set these to your desired softAP credentials. They are not configurable at runtime */
+const char *softAP_ssid = "ESP_ap";
+const char *softAP_password = "A6";
+
+/* hostname for mDNS. Should work at least on windows. Try http://esp8266.local */
+
+const char *myHostname = "freezer";
+
+/* Don't set this wifi credentials. They are configurated at runtime and stored on EEPROM */
+char ssid[32] = "";
+char password[32] = "";
+float newtemp=0.0f;
+
+//float temperatureData[50];
+
+// DNS server
+const byte DNS_PORT = 53;
+DNSServer dnsServer;
+
+/* Storage for SSID and password */
+Preferences preferences;
+
+/* Soft AP network parameters */
+IPAddress apIP(192, 168, 4, 1);
+IPAddress netMsk(255, 255, 255, 0);
+
+
+/** Should I connect to WLAN asap? */
+boolean connect;
+
+/** Last time I tried to connect to WLAN */
+long lastConnectTry = 0;
+
+/** Current WLAN status */
+int status = WL_IDLE_STATUS;
+
+
+float temp=0.0f;
+float oldtemp=temp;
+float diff=0.1f;
+bool is_active=false;
+
+const int freezer = 25;
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(freezer,OUTPUT);
+  preferences.begin("CapPortAdv", false);
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(softAP_ssid,softAP_password);
+  delay(500); // Without delay I've seen the IP address blank
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  /* Setup the DNS server redirecting all the domains to the apIP */  
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  /* Setup web pages: root, wifi config pages, SO captive portal detectors and not found. */
+  server.on("/", handleRoot);
+  server.on("/wifi", handleWifi);
+  server.on("/wifisave", handleWifiSave);
+  server.on("/set_temp",handleSetTemp);
+  server.on("/save_temp",handleSaveTemp);
+  //server.on("/get_graph",handleGraph);
+  server.on("/generate_204", handleRoot);  //Android captive portal. Maybe not needed. Might be handled by notFound handler.
+  server.on("/fwlink", handleRoot);  //Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
+  server.onNotFound ( handleNotFound );
+  /*server.on("/get_temperature_data", HTTP_GET, []() {
+        String jsonResponse = "[";
+        int dataSize= sizeof(temperatureData) / sizeof(temperatureData[0]);
+        for (int i = 0; i < dataSize; i++) {
+            jsonResponse += String(temperatureData[i]);
+            if (i < dataSize - 1) {
+                jsonResponse += ",";
+            }
+        }
+        jsonResponse += "]";
+        server.send(200, "application/json", jsonResponse);
+    });*/
+  server.begin(); // Web server start
+  Serial.println("HTTP server started");
+  //loadCredentials(); // Load WLAN credentials from network
+  //connect = strlen(ssid) > 0; // Request WLAN connect if there is a SSID
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+
+  thermo.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  updateOLED();
+}
+
+void connectWifi() {
+  Serial.println("Connecting as wifi client...");
+  WiFi.disconnect();
+  WiFi.begin ( ssid, password );
+  int connRes = WiFi.waitForConnectResult();
+  Serial.print ( "connRes: " );
+  Serial.println ( connRes );
+  
+}
+
+void loop() {
+  
+  if (connect) {
+    Serial.println ( "Connect requested" );
+    connect = false;
+    connectWifi();
+    lastConnectTry = millis();
+  }
+  else{
+    int s = WiFi.status();
+    if (s == 0 && millis() > (lastConnectTry + 60000) ) {
+      /* If WLAN disconnected and idle try to connect */
+      /* Don't set retry time too low as retry interfere the softAP operation */
+      connect = true;
+    }
+    if (status != s) { // WLAN status change
+      Serial.print ( "Status: " );
+      Serial.println ( s );
+      status = s;
+      if (s == WL_CONNECTED) {
+        /* Just connected to WLAN */
+        Serial.println ( "" );
+        Serial.print ( "Connected to " );
+        Serial.println ( ssid );
+        Serial.print ( "IP address: " );
+        Serial.println ( WiFi.localIP() );
+
+        // Setup MDNS responder
+        if (!MDNS.begin(myHostname)) {
+          Serial.println("Error setting up MDNS responder!");
+        } else {
+          Serial.println("mDNS responder started");
+          // Add service to MDNS-SD
+          MDNS.addService("http", "tcp", 80);
+        }
+      } else if (s == WL_NO_SSID_AVAIL) {
+        WiFi.disconnect();
+      }
+    }
+  }
+  
+  // Do work:
+  //DNS
+  dnsServer.processNextRequest();
+  //HTTP
+  server.handleClient();
+  updateRDT();
+  is_active=temp>newtemp;
+  if(temp>=oldtemp+diff||temp<=oldtemp-diff){
+    updateOLED();
+    oldtemp=temp;
+  }
+  if (is_active){
+    digitalWrite(freezer, HIGH);
+  }
+  else{
+    digitalWrite(freezer, LOW);
+  }
+}
+
+void updateOLED(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("Just Chillin' Freezer");
+  display.setCursor(0,10);
+  display.print("Setpoint:");
+  display.print(newtemp);
+  display.print(" *C");
+  display.setCursor(0,20);
+  display.print("Curent Temp:");
+  display.print(temp);
+  display.print(" *C");
+  display.display();
+}
+
+void updateRDT(){
+  temp=thermo.temperature(RNOMINAL, RREF);
+  //Serial.println(temp);
+  // Check and print any faults
+  uint8_t fault = thermo.readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo.clearFault();
+  }
+}
